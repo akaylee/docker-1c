4fe9484b3102295a6ec4bbc55aa5999d082ead88
akaylee <kateteslam83@gmail.com>
2022-09-12 18:36:25 +0300

    docker registry push

--- 1c-server/build.sh
+++ 1c-server/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build --tag 10.3.80.70:5000/1c-server:8.3.20.1996 .
+docker build --tag 1c-server:8.3.20.1996 .
+docker tag 1c-server:8.3.20.1996 10.3.80.70:5000/1c-server:8.3.20.1996
 docker push 1c-server:8.3.20.1996
