eda03d64306e475fee69ff5a639ecf0c58efd1d0
akaylee <kateteslam83@gmail.com>
2022-09-15 14:03:10 +0300

    run.sh for 1c

--- 1c-server/run.sh
+++ 1c-server/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # docker pull 10.3.80.70:5000/1c-server:8.3.20.1996
-docker run --name 10.3.80.70:5000/1c-server:8.3.20.1996 \
+docker run --name 1c-server\
   --net host \
   --detach \
   --restart=always \
   --volume 1c-server-home:/home/usr1cv8 \
   --volume 1c-server-logs:/var/log/1C \
   --volume /etc/localtime:/etc/localtime:ro \
-  1c-server
+  10.3.80.70:5000/1c-server:8.3.20.1996
