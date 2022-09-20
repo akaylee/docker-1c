c64a18d7a0892a85de3620dbef9c17f51d58b8b6
akaylee <kateteslam83@gmail.com>
2022-09-20 14:56:39 +0300

    creating image for 1cv8.3.19

--- 1c-server/build.sh
+++ 1c-server/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+SERVER_VERSION=8.3.19.1522
 
-set -e
-
-docker build --tag 10.3.80.70:5000/1c-server:{$SERVER_VERSION} .
-docker image push 10.3.80.70:5000/1c-server:{$SERVER_VERSION}
+docker build --tag 10.3.80.70:5000/1c-server:$SERVER_VERSION .
+docker image push 10.3.80.70:5000/1c-server:$SERVER_VERSION
