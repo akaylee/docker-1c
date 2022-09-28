89decf41788d4136ba2465bd1e02ca744d4810a1
akaylee <kateteslam83@gmail.com>
2022-09-28 13:31:58 +0300

    testing variables from build.sh

--- 1c-server/build.sh
+++ 1c-server/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PACKAGE_VERSION=8_3_19_1522
+PLATFORM_VERSION=8.3.19.1522
 
 docker build \
---build-arg PACKAGE_VERSION=$PACKAGE_VERSION \
+--build-arg PLATFORM_VERSION=$PLATFORM_VERSION \
 --build-arg SERVER_VERSION=8.3.19-1522 \
 --build-arg PLATFORM_VERSION=8.3.19.1522 \
---tag 10.3.80.70:5000/1c-server:$PACKAGE_VERSION .
+--tag 10.3.80.70:5000/1c-server:$PLATFORM_VERSION .
 
-docker image push 10.3.80.70:5000/1c-server:$PACKAGE_VERSION
+docker image push 10.3.80.70:5000/1c-server:$PLATFORM_VERSION
