15043a514f19ecc3784ae69ecc5d91bb3444ded0
akaylee <kateteslam83@gmail.com>
2022-09-28 16:18:53 +0300

    delete useless files

--- 1c-server/build.sh
+++ 1c-server/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PLATFORM_VERSION=8.3.19.1522
+PLATFORM_VERSION=8.3.18.1902
 
 docker build \
 --build-arg PLATFORM_VERSION=$PLATFORM_VERSION \
---build-arg SERVER_VERSION=8.3.19-1522 \
---build-arg PACKAGE_VERSION=8_3_19_1522 \
+--build-arg SERVER_VERSION=8.3.18-1902 \
+--build-arg PACKAGE_VERSION=8_3_18_1902 \
 --tag 10.3.80.70:5000/1c-server:$PLATFORM_VERSION .
 
 docker image push 10.3.80.70:5000/1c-server:$PLATFORM_VERSION
