1a06c94b70e7087eb2a0d7056072992522356f79
akaylee <kateteslam83@gmail.com>
2022-09-28 16:33:58 +0300

    check another version

--- 1c-server/build.sh
+++ 1c-server/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PLATFORM_VERSION=8.3.18.1902
+PLATFORM_VERSION=8.3.17.2665
 
 docker build \
 --build-arg PLATFORM_VERSION=$PLATFORM_VERSION \
---build-arg SERVER_VERSION=8.3.18-1902 \
---build-arg PACKAGE_VERSION=8_3_18_1902 \
+--build-arg SERVER_VERSION=8.3.17-2665 \
+--build-arg PACKAGE_VERSION=8_3_17_2665 \
 --tag 10.3.80.70:5000/1c-server:$PLATFORM_VERSION .
 
 docker image push 10.3.80.70:5000/1c-server:$PLATFORM_VERSION
